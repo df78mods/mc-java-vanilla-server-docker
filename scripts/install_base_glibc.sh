e35ee25f684b4c245e2e6b98dd0c207cb2d9b0fd
saucecake <48312022+DragonMeme@users.noreply.github.com>
2026-08-20 13:08:17 +1200

    Make mounted server files user agnostic
    - Default add a non-root user owned directory in server file directory.
      - Image now uses non-root user out of the box.
      - Includes adding in `eula.txt` option for image build.
    - Setup allows user to have their own directory in the mount folder.
      - Prevents potential file overwrite by other users.
    - Adapted docker compose to allow different user server-file mounts.

--- scripts/install_base_glibc.sh
+++ scripts/install_base_glibc.sh
@@ -28,5 +28,18 @@ cd ./root
 mkdir -p usr/local/bin mnt
 mkdir -m 1777 mnt/server-files
 mkdir -m 1777 tmp
+
+NON_ROOT_UID=65532
+NON_ROOT_GID=65532
+NON_ROOT_MOUNT_DIR=mnt/server-files/${NON_ROOT_UID}_${NON_ROOT_GID}
+mkdir $NON_ROOT_MOUNT_DIR
+chown $NON_ROOT_UID:$NON_ROOT_GID $NON_ROOT_MOUNT_DIR
+
+if [[ -n "${MINECRAFT_EULA}" ]]; then
+	EULA_FILE_LOC=$NON_ROOT_MOUNT_DIR/eula.txt
+	echo "eula=$MINECRAFT_EULA" > $EULA_FILE_LOC
+	chown $NON_ROOT_UID:$NON_ROOT_GID $EULA_FILE_LOC
+fi
+
 ln -s usr/lib lib
 ln -s usr/lib64 lib64
