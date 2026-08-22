ead93223abde96bfd4b6f6519a8ebe10d22d7185
saucecake <48312022+DragonMeme@users.noreply.github.com>
2026-08-22 23:09:33 +1200

    Patch ability to create os-release
    - Should now be able to add os-release file in musl build.

--- scripts/install_base_musl.sh
+++ scripts/install_base_musl.sh
@@ -7,7 +7,7 @@ mkdir root
 
 # Prepare the distroless base file system.
 cd ./root
-mkdir -p usr/local/bin usr/lib usr/lib64 mnt
+mkdir -p usr/local/bin usr/lib usr/lib64 mnt etc
 mkdir -m 1777 mnt/server-files
 mkdir -m 1777 tmp
 
